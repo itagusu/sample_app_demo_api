160d5ad137b54d5cb7b7272eb316ecb48a4d4de0
itagusu <ir060906@gmail.com>
2021-11-19 04:04:40 +0000

    JavaScript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,8 +17,36 @@
 //= require turbolinks
 //= require_tree .
 
+alert('Hello World');
+
+let hello = 'Hello World'
+alert(hello)
+
+let orange = 100;
+let apple = 120;
+
+if(orange > apple){
+  alert('みかんが高い');
+}else if(orange == apple){
+  alert('一緒');
+}else{
+  alert('リンゴが高い');
+}
+
+let max = 10000;
+let num = 1;
+let count = 0;
+
+while(num < max){
+  num = num*2;
+  count = count + 1;
+}
+
+alert('２を欠け続けて' + max + 'を超えるのは' + count + '回です')
+
 $(document).ready(function () {
   $('.jquery').on('click', function(){
     $(this).css('color','red');
   });
 });
+
